5da4d5437bdcd8a81a3ee27aea660e7825539890
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-05 12:19:30 +0200

    Fetch token

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,6 +1,13 @@
 import { getAllSites } from '@/lib/db-admin';
+import { auth } from "@/lib/firebase-admin"
 
-export default async (_, res) => {
+export default async (req, res) => {
+
+  const user = await auth.verifyIdToken(req.headers.token)
+
+  console.log(user)
+
+  const token = req.headers.token
 
   const { sites, error } = await getAllSites();
 
