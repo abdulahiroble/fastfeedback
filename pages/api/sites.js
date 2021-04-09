00c9add16f7394bce867f94a344c6868bcf65cce
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-09 20:45:17 +0200

    Redirect to dashboard if already logged in

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -6,8 +6,6 @@ export default async (req, res) => {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token)
 
-    console.log(uid)
-
     const { sites } = await getUserSites(uid);
 
     res.status(200).json({ sites });
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,13 @@ const Home = () => {
 
   return (
     <Flex as="main" direction="column" align="center" justify="center" h="100vh">
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          if (document.cookie && document.cookie.includes('fast-feedback-auth')) {
+            window.location.href = "/dashboard"
+          }
+        ` }} />
+
       <Head><title>Fast Feedback</title></Head>
 
       {/* <Icon color="black" name="logo" size="48px" mb={2} /> */}
@@ -24,7 +31,7 @@ const Home = () => {
         <Link href="https://react2025.com" isExternal textDecoration="underline">
           React 2025
         </Link>
-        {`. It's the easiest wat to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
+        {`. It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
       </Text>
       {auth.user ? (
         <Button
