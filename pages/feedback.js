e782dd9ac42aec905c956650ba8daf0ef7adfc40
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-19 13:49:32 +0200

    Build error

--- pages/feedback.js
+++ pages/feedback.js
@@ -2,10 +2,8 @@ import { useAuth } from '@/lib/auth'
 import EmptyState from "@/components/EmptyState"
 import DashboardShell from "@/components/DashboardShell"
 import useSWR from "swr"
-import FeedbackTableHeader from '@/components/FeedbackTableHeader';
 import fetcher from 'utils/fetcher';
 import FeedbackTableSkeleton from '@/components/FeedbackTableSkeleton';
-import SiteTableSkeleton from '@/components/SiteTableSkeleton'
 import FeedbackTable from '@/components/FeedbackTable';
 
 
