ce3f9e68407222eb50c769bfb138166cdaee904e
abdulahi roble <abdulahi_2610@hotmail.com>
2021-05-10 16:27:05 +0200

    Upgrade

--- pages/account.js
+++ pages/account.js
@@ -2,30 +2,47 @@ import { useAuth } from '@/lib/auth'
 import DashboardShell from "@/components/DashboardShell"
 import useSWR from "swr"
 import fetcher from 'utils/fetcher';
-import { createCheckoutSession } from '@/lib/db';
-import { Button } from '@chakra-ui/react';
+import { createCheckoutSession, goToBillingPortal } from '@/lib/db';
+import { Button, Box } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const Account = () => {
-    const { user } = useAuth();
-    // const { data } = useSWR(user ? ["/api/user", user.token] : null, fetcher)
+    const { user, signout } = useAuth();
+    const [isCheckoutLoading, setCheckoutloading] = useState(false);
+    const [isBillingLoading, setBillingLoading] = useState(false);
 
-    // if (!data) {
-    //     return <DashboardShell>
-    //         <SiteTableHeader />
-    //         <SiteTableSkeleton />
-    //     </DashboardShell>
     // }
 
     return (<DashboardShell>
-        <Button backgroundColor="gray.900"
-            color="white"
-            fontWeight="medium"
-            _hover={{ bg: 'gray.700' }}
-            _active={{
-                bg: 'gray.800',
-                transform: 'scale(0.95)'
-            }} mt={4} size="lg" onClick={(e) => createCheckoutSession(user.uid)}>Upgrade to Starter
-        </Button>
+        <Box>
+            <Button backgroundColor="gray.900"
+                color="white"
+                fontWeight="medium"
+                _hover={{ bg: 'gray.700' }}
+                isLoading={isCheckoutLoading}
+                _active={{
+                    bg: 'gray.800',
+                    transform: 'scale(0.95)'
+                }} mt={4} size="lg" onClick={(e) => {
+                    setCheckoutloading(true);
+                    createCheckoutSession(user.uid)
+                }}>Upgrade to Starter
+            </Button>
+            <Button backgroundColor="gray.900"
+                color="white"
+                fontWeight="medium"
+                ml={4}
+                isLoading={isBillingLoading}
+                _hover={{ bg: 'gray.700' }}
+                _active={{
+                    bg: 'gray.800',
+                    transform: 'scale(0.95)'
+                }} mt={4} size="lg" onClick={(e) => {
+                    setBillingLoading(true);
+                    goToBillingPortal()
+                }}> View Billing Portal
+            </Button>
+        </Box>
     </DashboardShell>)
 
 
