0864d4d7cda5c5314852daab92945d47b0049a08
abdulahi roble <abdulahi_2610@hotmail.com>
2021-05-29 19:42:18 +0200

    fix

--- pages/embed/[siteId].js
+++ pages/embed/[siteId].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { Box } from '@chakra-ui/core';
+import { Box } from '@chakra-ui/react';
 
 import Feedback from '@/components/Feedback';
 import FeedbackLink from '@/components/FeedbackLink';
